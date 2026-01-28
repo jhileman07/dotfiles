bf62d6fc87c6e2ce55a958286b44eb02b96ca440
John Hileman <johnhileman2026@u.northwestern.edu>
2026-01-28 05:10:20 -0600

    nvim fix for treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,43 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			-- core langs you asked for
-			"cpp",
-			"c",
-			"python",
-			"rust",
-			"lua",
-			-- infra & config you’ll touch constantly
-			"cmake",
-			"make",
-			"bash",
-			"regex",
-			"json",
-			"jsonc",
-			"yaml",
-			"toml",
-			"markdown",
-			"markdown_inline",
-			"query",
-			"vim",
-			"vimdoc",
-			"gitcommit",
-			"diff",
-			-- nice to have for quant stacks
-			"dockerfile",
-			"sql",
-			"cuda",
-			"proto",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			disable = function(lang, buf)
-				local max = 300 * 1024 -- 300 KB for gigantic vendor files
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				return (ok and stats and stats.size > max) or false
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "cpp",
+      "c",
+      "python",
+      "rust",
+      "lua",
+      "cmake",
+      "make",
+      "bash",
+      "regex",
+      "json",
+      "jsonc",
+      "yaml",
+      "toml",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "vim",
+      "vimdoc",
+      "gitcommit",
+      "diff",
+      "dockerfile",
+      "sql",
+      "cuda",
+      "proto",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      disable = function(lang, buf)
+        local max = 300 * 1024 -- 300 KB for gigantic vendor files
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        return (ok and stats and stats.size > max) or false
+      end,
+      additional_vim_regex_highlighting = false,
+    },
+  },
 }
